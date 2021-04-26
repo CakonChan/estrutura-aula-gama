226e2aa4b573c4c7d138f57d71122f8f93c2ef94
CakonChan <caioferrazporto@hotmail.com>
2021-04-26 18:54:59 -0300

    implementação para busca

--- busca.js
+++ busca.js
@@ -1,5 +1,5 @@
-var valores = [5, 8, 10, 22, 45, 38];
-  //posição =  0  1   2   3   4   5
+var valores = [5, 8, 10, 22, 38, 45, 60, 91, 100, 120];
+  //posição =  0  1   2   3   4   5   6   7    8    9
 
   // Caso busca é "100" então retorno seria -1 ou null depende da situação.
 
@@ -11,6 +11,29 @@ function busca(num){
     }
     return -1;
 }
+
+function buscaBin(num) {
+    let inicio, meio, fim;
+    
+    inicio = 0;
+    fim = 9;
+
+    while(inicio < fim){
+        meio = (inicio + fim) / 2;
+        if(num == valores[meio]){
+            return meio;
+        }else{
+            if(num > valores[meio]){
+                inicio = meio + 1;
+            }else{
+                fim = meio - 1;
+            }
+        }
+
+    }
+
+}
+
 
 // usando a nossa ferramenta de busca
 
